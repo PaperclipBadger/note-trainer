c55baf237fc0d8aced26b4fb47d3e159d439cdd6
Blaine Rogers <blaine.w.rogers@gmail.com>
2023-08-07 08:15:32 +0100

    Make melodies optionally repetetive.

--- notetrainer/app.py
+++ notetrainer/app.py
@@ -31,6 +31,7 @@ parser.add_argument('mode', type=parse_mode, help='mode of scale')
 parser.add_argument('chords', nargs='+', help='4 chord progression, by degree')
 parser.add_argument('--bpm', type=float, default=60.0, help='beats per minute')
 parser.add_argument('--n-bars', type=int, default=8, help='number of bars to preview')
+parser.add_argument('--melody-length', type=int, default=0, help='melody pattern will change every n bars')
 args = parser.parse_args()
 
 
@@ -48,8 +49,20 @@ def get_chord(bar):
     chord = chords[bar % len(chords)]
     return f'{chord.guitar_notation():7s}|'
 
+melody = None
+
 def get_melody(bar):
-    notes = [random.choice((1, 3, 5, 7)) for _ in range(4)]
+    global melody
+
+    if not args.melody_length:
+        notes = [random.choice((1, 3, 5, 7)) for _ in range(4)]
+    else:
+        mod = bar % args.melody_length
+
+        if mod == 0:
+            melody = [random.choice((1, 3, 5, 7)) for _ in range(4)]
+
+        notes = melody
 
     return ' '.join(map(str, notes)) + '|'
 
@@ -62,11 +75,29 @@ print()
 for i in itertools.count():
     print('\r\033[2A', end='')
 
-    print(chord_buffer[i % 8:i % 8 + 8 * args.n_bars])
-    print(melody_buffer[i % 8:i % 8 + 8 * args.n_bars])
-    time.sleep(30.0 / args.bpm)
+    chord_sheet = chord_buffer[i % 8:i % 8 + 8 * args.n_bars]
+    melody_sheet = melody_buffer[i % 8:i % 8 + 8 * args.n_bars]
+
+    print(
+        chord_sheet[:8],
+        '\u001b[41m\u001b[37m',
+        chord_sheet[8],
+        '\u001b[0m',
+        chord_sheet[9:],
+        sep='',
+    )
+    print(
+        melody_sheet[:8],
+        '\u001b[41m\u001b[37m',
+        melody_sheet[8],
+        '\u001b[0m',
+        melody_sheet[9:],
+        sep='',
+    )
 
     div, mod = divmod(i + 1, 8)
     if mod == 0:
         chord_buffer = chord_buffer[8:] + get_chord(div + args.n_bars + 1)
         melody_buffer = melody_buffer[8:] + get_melody(div + args.n_bars + 1)
+
+    time.sleep(30.0 / args.bpm)
